c3e2ea120322b39dd619b06f57b93a27e1fe9dc4
Felipe Borba <felipe.borba0109@gmail.com>
2024-11-20 15:37:20 -0300

    add old files

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,32 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 
+import 'Login.dart';
+import 'RouteGenerator.dart';
+
+final ThemeData temaIOS = ThemeData(
+  primaryColor: Colors.grey[200],
+  colorScheme: ColorScheme.fromSeed(
+    seedColor: const Color(0xff075E54),
+    secondary: const Color(0xff25D366),
+  ),
+);
+
+final ThemeData temaPadrao = ThemeData(
+  primaryColor: Color(0xff075E54),
+  colorScheme: ColorScheme.fromSeed(
+    seedColor: const Color(0xff075E54),
+    secondary: const Color(0xff25D366),
+  ),
+);
+
 void main() {
-  runApp(const MainApp());
-}
-
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
-    );
-  }
+  runApp(MaterialApp(
+    home: Login(),
+    theme: Platform.isIOS ? temaIOS : temaPadrao,
+    initialRoute: "/",
+    onGenerateRoute: RouteGenerator.generateRoute,
+    debugShowCheckedModeBanner: false,
+  ));
 }
